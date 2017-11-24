cb454ef64ba9a4cb870549c0f7593fdd9a14547a
garrsaun <garrsaun253@ed.amdsb.com>
2017-11-24 13:06:46 -0500

    Add project files.

---
+++ DrawTriangle/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DrawTriangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xBox1 = new System.Windows.Forms.TextBox();
+            this.yBox3 = new System.Windows.Forms.TextBox();
+            this.yBox2 = new System.Windows.Forms.TextBox();
+            this.yBox1 = new System.Windows.Forms.TextBox();
+            this.xBox3 = new System.Windows.Forms.TextBox();
+            this.xBox2 = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // xBox1
+            // 
+            this.xBox1.Location = new System.Drawing.Point(12, 12);
+            this.xBox1.Name = "xBox1";
+            this.xBox1.Size = new System.Drawing.Size(49, 20);
+            this.xBox1.TabIndex = 0;
+            // 
+            // yBox3
+            // 
+            this.yBox3.Location = new System.Drawing.Point(67, 64);
+            this.yBox3.Name = "yBox3";
+            this.yBox3.Size = new System.Drawing.Size(49, 20);
+            this.yBox3.TabIndex = 1;
+            // 
+            // yBox2
+            // 
+            this.yBox2.Location = new System.Drawing.Point(67, 38);
+            this.yBox2.Name = "yBox2";
+            this.yBox2.Size = new System.Drawing.Size(49, 20);
+            this.yBox2.TabIndex = 2;
+            // 
+            // yBox1
+            // 
+            this.yBox1.Location = new System.Drawing.Point(67, 12);
+            this.yBox1.Name = "yBox1";
+            this.yBox1.Size = new System.Drawing.Size(49, 20);
+            this.yBox1.TabIndex = 3;
+            // 
+            // xBox3
+            // 
+            this.xBox3.Location = new System.Drawing.Point(12, 64);
+            this.xBox3.Name = "xBox3";
+            this.xBox3.Size = new System.Drawing.Size(49, 20);
+            this.xBox3.TabIndex = 4;
+            // 
+            // xBox2
+            // 
+            this.xBox2.Location = new System.Drawing.Point(12, 38);
+            this.xBox2.Name = "xBox2";
+            this.xBox2.Size = new System.Drawing.Size(49, 20);
+            this.xBox2.TabIndex = 5;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(25, 90);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 6;
+            this.goButton.Text = "button1";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 329);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.xBox2);
+            this.Controls.Add(this.xBox3);
+            this.Controls.Add(this.yBox1);
+            this.Controls.Add(this.yBox2);
+            this.Controls.Add(this.yBox3);
+            this.Controls.Add(this.xBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox xBox1;
+        private System.Windows.Forms.TextBox yBox3;
+        private System.Windows.Forms.TextBox yBox2;
+        private System.Windows.Forms.TextBox yBox1;
+        private System.Windows.Forms.TextBox xBox3;
+        private System.Windows.Forms.TextBox xBox2;
+        private System.Windows.Forms.Button goButton;
+    }
+}
+
---
+++ DrawTriangle/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawTriangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            int x1 = Convert.ToInt16(xBox1.Text);
+            int x2 = Convert.ToInt16(xBox2.Text);
+            int x3 = Convert.ToInt16(xBox3.Text);
+            int y1 = Convert.ToInt16(yBox1.Text);
+            int y2 = Convert.ToInt16(yBox2.Text);
+            int y3 = Convert.ToInt16(yBox3.Text);
+
+            Pen pen = new Pen(Color.Red, 1);
+
+            DrawTriangle(pen, x1, y1, x2, y2, x3, y3);
+        }
+
+        //Draw Triangle Method
+        public void DrawTriangle(Pen pen, int x1, int y1, int x2, int y2, int x3, int y3)
+        {
+            Graphics g = this.CreateGraphics();
+
+            g.DrawLine(pen, x1, y1, x2, y2);
+            g.DrawLine(pen, x2, y2, x3, y3);
+            g.DrawLine(pen, x3, y3, x1, y1);
+        }
+
+        
+    }
+}
